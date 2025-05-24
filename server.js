fafaa0534179280c459303e980f66d44d921c900
Hrishabh ranjan <hrishabhvik@gmail.com>
2025-05-24 20:32:26 +0530

    First commit

--- server.js
+++ server.js
@@ -10,8 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Connect to MongoDB
 mongoose.connect("mongodb://localhost:27017/todo", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+  
 }).then(() => {
     console.log("Connected to MongoDB");
 }).catch((err) => {
